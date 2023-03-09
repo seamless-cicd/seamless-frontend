14981729e3073820fdec8a01a3512c769754887a
Ethan Weiner <ethanweiner20@gmail.com>
2023-03-09 11:48:02 -0500

    feat: rename logs directory

--- src/components/run/StageCard.tsx
+++ src/components/run/StageCard.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StageCardProps } from '../../schema/stageSchema';
-import Logs from '../logs/Logs';
+import Logs from '../logs_display/Logs';
 
 const submitButtonStyle =
   'mt-4 mr-2 bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded';
