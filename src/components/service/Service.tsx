a41f957e356c5d348561b1146e7eb64c31efa185
Ethan Weiner <ethanweiner20@gmail.com>
2023-03-09 11:38:09 -0500

    feat: display simple logs

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -1,34 +1,34 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { RunType } from "../../schema/runSchema";
-import RunsList from "./RunsList";
-import ServiceHeaderCard from "./ServiceHeaderCard";
-import { ServiceType } from "../../schema/serviceSchema";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { RunType } from '../../schema/runSchema';
+import RunsList from './RunsList';
+import ServiceHeaderCard from './ServiceHeaderCard';
+import { ServiceType } from '../../schema/serviceSchema';
 import axios from 'axios';
 
 const TEST_RUNS_URL = import.meta.env.VITE_TEST_RUNS_URL;
 const TEST_SERVICES_URL = import.meta.env.VITE_TEST_SERVICES_URL;
 
 const defaultService = {
-  id: "",
-  createdAt: new Date,
-  updatedAt: new Date,
-  name: "",
-  lastRunAt: new Date,
+  id: '',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  name: '',
+  lastRunAt: new Date(),
   triggerOnMain: false,
   triggerOnPrOpen: false,
   triggerOnPrSync: false,
   useStaging: false,
   autoDeploy: false,
-  githubRepoUrl: "",
-  unitTestCommand: "",
-  integrationTestCommand: "",
-  codeQualityCommand: "",
-  dockerfilePath: "",
-  dockerComposeFilePath: "",
-  pipelineId: "",
+  githubRepoUrl: '',
+  unitTestCommand: '',
+  integrationTestCommand: '',
+  codeQualityCommand: '',
+  dockerfilePath: '',
+  dockerComposeFilePath: '',
+  pipelineId: '',
   runs: [],
-}
+};
 
 const Service = () => {
   const serviceId = useParams().serviceId;
@@ -39,30 +39,35 @@ const Service = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const runsRequest = axios.get(TEST_RUNS_URL + `?serviceId=${serviceId}`);
+        const runsRequest = axios.get(
+          TEST_RUNS_URL + `?serviceId=${serviceId}`
+        );
         const serviceRequest = axios.get(TEST_SERVICES_URL + serviceId);
 
-        const [runsResponse, serviceResponse] = await axios.all([runsRequest, serviceRequest]);
+        const [runsResponse, serviceResponse] = await axios.all([
+          runsRequest,
+          serviceRequest,
+        ]);
 
         setRuns(runsResponse.data);
         setService(serviceResponse.data);
       } catch (e) {
         console.log(e);
       }
-    }
+    };
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div className="mt-8 ml-8">
-    <h2 className="text-3xl text-indigo-700 font-extrabold mb-4">Service</h2>
-    
-    <ServiceHeaderCard service={service} />
-    <div className="border rounded-lg shadow-md p-4 mr-80">
-      <RunsList runs={runs} setRuns={setRuns} /> 
-    </div>
-  </div>
-  )
-}
+      <h2 className="text-3xl text-indigo-700 font-extrabold mb-4">Service</h2>
 
-export default Service
+      <ServiceHeaderCard service={service} />
+      <div className="border rounded-lg shadow-md p-4 mr-80">
+        <RunsList runs={runs} setRuns={setRuns} />
+      </div>
+    </div>
+  );
+};
+
+export default Service;
